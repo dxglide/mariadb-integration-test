dd3108aae88a3b59634a394db551400eabfb580a
dxglide <dxglide@gmail.com>
2021-02-18 21:23:10 +0200

    initial

---
+++ src/main/java/org/dxglide/tests/springboot/dbintegrations/testing/DbIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package org.dxglide.tests.springboot.dbintegrations.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbIntegrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbIntegrationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dxglide/tests/springboot/dbintegrations/testing/DbIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dxglide.tests.springboot.dbintegrations.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbIntegrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
